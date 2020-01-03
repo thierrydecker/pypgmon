f5a5ad9174af5ebe1e2a32eb58f3c3c24fc38651
Thierry DECKER <mail@thierry-decker.com>
2020-01-03 18:12:54 +0100

    Add configure_app()

---
+++ pypgmon/helpers.py
@@ -0,0 +1,23 @@
+"""Helpers module.
+
+Copyright (c) 2020 Thierry P.G. DECKER
+All Rights Reserved.
+Released under the MIT license
+
+"""
+
+
+def configure_app():
+    """PyPgMon configuration
+
+    Args:
+
+    Returns:
+        Either True if configuration could be done
+        Or False if configuration could not be done
+
+    Raises:
+
+    """
+    configuration_done = True
+    return configuration_done
