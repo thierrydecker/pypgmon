ea9fc43e0f504f6a7cb9ac335e55d457f65aedb3
Thierry DECKER <mail@thierry-decker.com>
2020-01-06 16:43:17 +0100

    Add scheduler

--- pypgmon/jobs.py
+++ pypgmon/jobs.py
@@ -5,3 +5,38 @@ All Rights Reserved.
 Released under the MIT license
 
 """
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def create_scheduler(configuration):
+    """Start scheduler
+
+    Args:
+        configuration: A valid configuration dictionnary
+
+    Returns:
+        A scheduler or None
+
+    Raises:
+
+    """
+    try:
+        max_workers = configuration['scheduler']['max_workers']
+        scheduler = BackgroundScheduler(
+                executors={
+                    'default':
+                        {
+                            'type': 'threadpool',
+                            'max_workers': max_workers,
+                        },
+                }
+        )
+        logger = logging.getLogger(__name__)
+        logger.info(f'Scheduler created for {max_workers} workers')
+    except ValueError as e:
+        scheduler = None
+        logger = logging.getLogger(__name__)
+        logger.error(f'Creating the scheduler, {e}')
+    return scheduler
