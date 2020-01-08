1b08b7d5f3f936c3273a7fdbc79a1dd60052987e
Thierry DECKER <mail@thierry-decker.com>
2020-01-08 08:22:57 +0100

    Implement base pg_stat_database() Implement base pg_stat_all_tables()

--- pypgmon/jobs.py
+++ pypgmon/jobs.py
@@ -6,8 +6,8 @@ Released under the MIT license
 
 """
 import logging
-import random
-import time
+
+import psycopg2
 
 
 def pg_stat_database(
@@ -22,13 +22,13 @@ def pg_stat_database(
     """pg_stat_database job
 
     Args:
-        cluster:
-        description:
-        host:
-        port:
-        dbname:
-        user:
-        password:
+        cluster: Cluster's key
+        description: Cluster's description
+        host: Cluster's hostname or ip address
+        port:Cluster's port
+        dbname: Cluster's dbname
+        user: Cluster's user
+        password: Cluster's user password
 
     Returns:
 
@@ -37,8 +37,20 @@ def pg_stat_database(
     """
     logger = logging.getLogger(__name__)
     logger.debug(f'Polling cluster {cluster} ({description})...')
-    time.sleep(random.randint(0, 4))
-    logger.info(f'Cluster {cluster} ({description}) polled')
+    conn = None
+    try:
+        conn = psycopg2.connect(
+                host=host,
+                port=port,
+                dbname=dbname,
+                user=user,
+                password=password,
+        )
+        conn.close()
+        logger.info(f'Cluster {cluster} ({description}) polled')
+    except psycopg2.OperationalError as e:
+        logger.error(f'Could not poll cluster {host}:{port}')
+        logger.error({e})
 
 
 def pg_stat_all_tables(
@@ -68,5 +80,17 @@ def pg_stat_all_tables(
     """
     logger = logging.getLogger(__name__)
     logger.debug(f'Polling database {database} ({description})...')
-    time.sleep(random.randint(0, 4))
-    logger.info(f'Database {database} ({description}) polled')
+    conn = None
+    try:
+        conn = psycopg2.connect(
+                host=host,
+                port=port,
+                dbname=dbname,
+                user=user,
+                password=password,
+        )
+        conn.close()
+        logger.info(f'Database {database} ({description}) polled')
+    except psycopg2.OperationalError as e:
+        logger.error(f'Could not poll database {host}:{port}')
+        logger.error({e})
